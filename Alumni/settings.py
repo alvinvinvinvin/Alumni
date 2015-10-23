0c0153d5f786dd0cdfe507da570c8c8725dc1167
Han Chen <alvinchen8611@gmail.com>
2015-10-22 23:41:28 -0500

    Changed the pswd of database

--- Alumni/settings.py
+++ Alumni/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'NAME': 'mse_alumni',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '8078979ch',
+        'PASSWORD': '123456Mn!',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
